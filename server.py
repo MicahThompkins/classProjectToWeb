d7cb3b811d1bd5f64398f1fc12763be56ad5a8ca
MICAH THOMPKINS <micahthompkins1@gmail.com>
2021-04-14 23:20:27 -0500

    adding cors functionality

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
+from flask_cors import CORS
 from scan import ScanClass
 app = Flask(__name__)
+CORS(app)
 # scanner = ScanClass()
 
 @app.route('/', methods=['POST'])
